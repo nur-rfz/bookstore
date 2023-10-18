d4720852acf3960d496c34f905a1ff89f3b14c80
Jan Vaehsen <jvaehsen@deloitte.de>
2023-10-18 13:28:55 +0200

    improve error handling

--- controllers/books.js
+++ controllers/books.js
@@ -1,49 +1,59 @@
 let db = require('../config/database.js')
 const newBook = (req, res) => {
     db.run(`INSERT INTO books (title, author) VALUES (?, ?)`, [req.body.title, req.body.author], function (err) {
-        if (err) throw err;
-        res.statusCode = 201;
-        res.send({ id: this.lastID, ...req.body });
+        if (err) {
+            res.status(500)
+                .json({"error":err.message})
+                .send();
+        } else {
+            res.status(201)
+                .send({ id: this.lastID, ...req.body })
+        }
     });
 };
 
 const getBooks = (req, res) => {
     db.all(`SELECT * FROM books`, [], (err, rows) => {
-        if (err) throw err;
-        if (rows.length === 0) {
-            res.statusCode=404
-            res.send();
-            return;
+        if (err) {
+            res.status(500)
+                .json({"error":err.message})
+                .send();
+        } else if (rows.length === 0) {
+            res.status(404)
+                .send();
+        } else {
+            res.send(rows);
         }
-        res.send(rows);
     });
 };
 
 const getBookById = (req, res) => {
     db.get('SELECT * FROM books WHERE id = ?', [req.params.id], (err, row) => {
         if (err) {
-            res.status(400).json({"error":err.message});
-            res.send()
-            return;
+            res.status(500)
+                .json({"error":err.message})
+                .send();
+        } else if (row === undefined) {
+            res.status(404)
+                .send();
+        } else {
+            res.json(row)
+                .send();
         }
-        if (row === undefined) {
-            res.statusCode=404
-            res.send();
-            return;
-        }
-        res.json(row);
-
     });
 }
 
 const deleteBook = (req, res) => {
     db.run('DELETE FROM books WHERE id = ?', req.params.id, (err) => {
         if (err) {
-            res.status(400).json({"error":err.message});
-            return;
+            res.status(500)
+                .json({"error":err.message})
+                .send();
+        } else {
+            res.status(204)
+                .send();
         }
-        res.statusCode=204
-        res.send()
+
     });
 }
 
@@ -53,12 +63,14 @@ const updateBook = (req, res) => {
         `UPDATE books SET title = ?, author = ? WHERE id = ?`,
         [data.title, data.author, req.params.id],
         (err) => {
-            if (err){
-                res.status(400).json({"error": res.message});
-                return;
+            if (err) {
+                res.status(500)
+                    .json({"error":err.message})
+                    .send();
+            } else {
+                res.status(204)
+                    .send();
             }
-            res.statusCode=204
-            res.send()
         });
 }
 
